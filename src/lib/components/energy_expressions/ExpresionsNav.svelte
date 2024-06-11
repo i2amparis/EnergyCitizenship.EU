4f1ee54518f658732efe0033ebbf154d8caa3d8c
steftsotras <steftsotras2@gmail.com>
2024-06-11 21:32:48 +0300

    about highlight

--- src/lib/components/energy_expressions/ExpresionsNav.svelte
+++ src/lib/components/energy_expressions/ExpresionsNav.svelte
@@ -10,7 +10,14 @@
     const expressions_pages = [Access, Consumption, Production, Governance];
 
     let selectedPage = expressions_pages[0];
-    const loadPage = (obj) => selectedPage = obj;
+    let activeIndex = 0;
+    
+    // const loadPage = (obj) => selectedPage = obj;
+
+    function loadPage(page, index) {
+        selectedPage = page;
+        activeIndex = index;
+    }
 
 </script>
 
@@ -19,9 +26,19 @@
     <ul class="flex-column space-y space-y-4 text-md font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
         {#each main_categories as category, i}
             <li class=" min-w-fit">
-                <button 
+                <!-- <button 
                     on:click={() => loadPage(expressions_pages[i])}
                     class="inline-flex items-center px-4 py-3 hover:text-gray-900 bg-gray-50 hover:bg-gray-200 rounded-lg active w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white" aria-current="page">
+                    <img class="w-12 scale-50 min-[370px]:scale-75 sm:scale-90" src={category.imageUrl} alt='temp' />
+                    {category.name}
+                </button> -->
+                <button 
+                    on:click={() => loadPage(expressions_pages[i], i)}
+                    class="inline-flex items-center px-4 py-3 rounded-lg w-full
+                    {i === activeIndex 
+                        ? 'text-gray-900 bg-gray-200 dark:bg-gray-700 dark:text-white' 
+                        : 'hover:text-gray-900 bg-gray-50 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white'}" 
+                    aria-current="page">
                     <img class="w-12 scale-50 min-[370px]:scale-75 sm:scale-90" src={category.imageUrl} alt='temp' />
                     {category.name}
                 </button>
