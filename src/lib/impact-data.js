00562497b2a20144d88b3ee8e5354ca08f5d07c2
George Xexakis <gxexakis@gmail.com>
2024-06-12 23:29:10 +0200

    Add the rest of text on resources and impacts sections

---
+++ src/lib/impact-data.js
@@ -0,0 +1,68 @@
+// import module1_bg from "$lib/assets/cards/module1_bg.svg"
+// import module2_bg from "$lib/assets/cards/module2_bg.svg"
+
+
+let impact_cards = [
+    {
+        category: "Modelling",
+        title: 'Impact of energy citizenship on the local level',
+        // imageUrl: module2_bg,
+        description: 'A model-based analysis on the profitability of small-scale PV systems, on the growth of \
+            Collective Energy Initiatives, and on the impacts of a citizen-led transition',
+        href: 'https://encludeproject.eu/sites/default/files/2024-05/ENCLUDE_D5.3%20Report%20on%20the%20impact%20of%20energy%20citizenship%20at%20the%20local%20level_FINAL.pdf',
+        badges: [
+            {
+                title:"Academics",
+                color:"#4281A4"
+            },
+            {
+                title:"Policymakers",
+                color:"#EDB230"
+            }
+        ],
+        authors: [
+            "ENCLUDE"
+        ]
+    },
+    {
+        category: "Explore",
+        title: 'Development of decarbonization pathways based on social innovations of energy citizenship',
+        // imageUrl: module2_bg,
+        description: 'Narratives of energy citizenship based on the Shared Socioeconomic Pathways used in IPCC reports',
+        href: 'https://encludeproject.eu/sites/default/files/2023-02/ENCLUDE_D5.2%20Report%20on%20development%20of%20decarbonization%20pathways%20based%20on%20social%20innovations%20of%20energy%20citizenship.pdf',
+        badges: [
+            {
+                title:"Academics",
+                color:"#4281A4"
+            },
+        ],
+        authors: [
+            "ENCLUDE"
+        ]
+    },
+    {
+        category: "Explore",
+        title: 'Prospective energy citizenship scenarios',
+        // imageUrl: module2_bg,
+        description: 'Scenarios of energy citizenship development in the EU until 2030 based on an online survey of 10,071 citizens from 19 European countries',
+        href: 'https://www.energyprospects.eu/fileadmin/user_upload/lu_portal/www.energycitizen.eu/EnergyPROSPECTS_D5.5_31_03_2024_final.pdf',
+        badges: [
+            {
+                title:"Academics",
+                color:"#4281A4"
+            },
+            {
+                title:"Policymakers",
+                color:"#EDB230"
+            }
+        ],
+        authors: [
+            "ENERGY PROSPECTS"
+        ]
+    }
+];
+
+
+export {
+    impact_cards
+}
